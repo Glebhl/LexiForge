8229472bcd15c51566d37861f5baa7a912af2746
Glebhl <glebdriapko@gmail.com>
2026-04-28 17:58:35 +0300

    Mobile UI improvements

--- src/ui/views/lesson_setup/hint.js
+++ src/ui/views/lesson_setup/hint.js
@@ -3,15 +3,15 @@ const elements = {
 };
 
 const hints = [
-    "specify your level and goal (<code>A2 travel</code>, <code>B1 conversation</code>).",
-    "choose a topic and format (<code>food vocabulary</code>, <code>short sentences</code>).",
-    "include the situation (<code>at the airport</code>, <code>doctor appointment</code>).",
-    "request difficulty and pace (<code>simple sentences</code>, <code>challenge me</code>).",
-    "focus on a grammar point (<code>present perfect</code>, <code>conditionals</code>).",
-    "set the number of new words (<code>teach 10 B2 words</code>, <code>5 new B1 words</code>).",
-    "pick a register (<code>formal</code>, <code>casual</code>, <code>business</code>).",
-    "ask for phrasal verbs by theme (<code>phrasal verbs for work</code>, <code>for travel</code>).",
-    "include your interests (<code>music</code>, <code>gaming</code>, <code>fitness</code>).",
+    "Specify your level and goal (<code>A2 travel</code>, <code>B1 conversation</code>).",
+    "Choose a topic and format (<code>food vocabulary</code>, <code>short sentences</code>).",
+    "Include the situation (<code>at the airport</code>, <code>doctor appointment</code>).",
+    "Request difficulty and pace (<code>simple sentences</code>, <code>challenge me</code>).",
+    "Focus on a grammar point (<code>present perfect</code>, <code>conditionals</code>).",
+    "Set the number of new words (<code>teach 10 B2 words</code>, <code>5 new B1 words</code>).",
+    "Pick a register (<code>formal</code>, <code>casual</code>, <code>business</code>).",
+    "Ask for phrasal verbs by theme (<code>phrasal verbs for work</code>, <code>for travel</code>).",
+    "Include your interests (<code>music</code>, <code>gaming</code>, <code>fitness</code>).",
 ];
 
 function getRandomInt(min, max) {
